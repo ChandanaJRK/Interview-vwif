7f5e8c09ef2994e192fa7763e63376de573475b3
Chandana <134969744+ChandanaJRK@users.noreply.github.com>
2023-05-31 01:49:56 +0530

    Update GeoData_ObserverPattren_Boostlib.cpp

--- GeoData_ObserverPattren_Boostlib.cpp
+++ GeoData_ObserverPattren_Boostlib.cpp
@@ -18,7 +18,7 @@
 
 struct GeoData {
 
-    std::string type;
+    std::string location;
     double latitude;
     double longitude;
 };
@@ -29,12 +29,12 @@ public:
 
     using GeoDataSignal = boost::signals2::signal<void(const GeoData&)>;
 
-    boost::signals2::connection registerConsumer(GeoDataSignal::slot_type subscriber) {
+    boost::signals2::connection registerSubscriber(GeoDataSignal::slot_type subscriber) {
 
         return subscribers.connect(subscriber);
     }
 
-    void unregisterConsumer(boost::signals2::connection& connection) {
+    void deregisterSubscriber(boost::signals2::connection& connection) {
 
         connection.disconnect();
     }
@@ -55,29 +55,29 @@ public:
 
     void processGeoData(const GeoData& data) {
 
-        std::cout << "Updated GeoData Location: \n" << data.type << " \nLatitude in degree: " << data.latitude <<  "\nLongitude in degree: " << data.longitude <<"\n"<< std::endl;
+        std::cout << "Updated GeoData Location: \n" << data.location << " \nLatitude in degree: " << data.latitude <<  "\nLongitude in degree: " << data.longitude <<"\n"<< std::endl;
     }
 };
 
 int main() {
 
-    GeoData geoData1{"Area:  RensingStraße VWIF Bochum", 51.515522, 7.207843};
-    GeoData geoData2{"Area:  WittenerStraße VWIF Bochum", 51.473019, 7.242687};
+    GeoData geoData1{"Location:  RensingStraße VWIF Bochum", 51.515522, 7.207843};
+    GeoData geoData2{"Location:  WittenerStraße VWIF Bochum", 51.473019, 7.242687};
 
     AccessLibrary accessLibrary;
     MapRenderer renderer1, renderer2;
 
     // Subscribe to the publisher for geo data signal in the access library
-    boost::signals2::connection connection1 = accessLibrary.registerConsumer(boost::bind(&MapRenderer::processGeoData, &renderer1, _1));
-    boost::signals2::connection connection2 = accessLibrary.registerConsumer(boost::bind(&MapRenderer::processGeoData, &renderer2, _1));
+    boost::signals2::connection connection1 = accessLibrary.registerSubscriber(boost::bind(&MapRenderer::processGeoData, &renderer1, _1));
+    boost::signals2::connection connection2 = accessLibrary.registerSubscriber(boost::bind(&MapRenderer::processGeoData, &renderer2, _1));
 
     // Notify subscribers about new geo data    
     accessLibrary.notifySubscribers(geoData1);
     accessLibrary.notifySubscribers(geoData2);
 
     // Disconnect the consumer from receiving updates
-    accessLibrary.unregisterConsumer(connection1);
-    accessLibrary.unregisterConsumer(connection2);
+    accessLibrary.deregisterSubscriber(connection1);
+    accessLibrary.deregisterSubscriber(connection2);
 
     return 0;
 }
